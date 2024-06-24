c22de6501730b95b17722361e50f336e3aadf26a
odorajbotoj <odoraj_botoj@tom.com>
2024-06-24 20:49:04 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,25 +1,23 @@
-# mcbe-sapi-ts-template
+# FreeCamera
 
-MinecraftBE ScriptAPI Typescript project template
+MinecraftBE ScriptAPI FreeCamera
 
 ## Usage
 
-+ Run `npm i` to install packages
-+ Edit `behavior_pack/manifest.json`
-+ Edit `package.json`
-+ Change `behavior_pack/pack_icon.png`
-+ Write Typescript scripts in `scripts/`
-+ Write `CHANGELOG.md`
-+ Run `npm run pack` to build the `mcpack`
+1. 打开 `测试版API`
+2. 装载该pack
 
 ## Commands
 
-+ `npm run clean` to clean the workspace
-+ `npm run build` to build the typescript scripts
-+ `npm run cleanLocalDeploy` to remove the pack in `%localappdata%\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs`
-+ `npm run localDeploy` to generate the pack in `%localappdata%\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_behavior_packs`
-+ `npm run pack` to generate the mcpack
++ `+fc <speed: number>` 以speed为映射速度启用FreeCamera
++ `+fc` 禁用FreeCamera
+
+## Tips
+
++ 自由视角朝向将与玩家朝向保持一致，速度为玩家速度的speed倍（这意味着高speed会导致控制困难）
++ 蹲下以下降自由视角，跳起以抬升自由视角（这意味着当您不在地面上时，如处于飞行状态下，视角会一直抬升）
++ 夜视等效果将不会作用于自由视角
 
 ## License
 
-+ CC0-1.0 License
++ MIT License
--- scripts/main.ts
+++ scripts/main.ts
@@ -1,1 +1,36 @@
-// build your world!
+//@ts-ignore
+import { world, ChatSendBeforeEvent, system, EasingType, Vector3 } from "@minecraft/server"
+
+//@ts-ignore
+world.beforeEvents.chatSend.subscribe((event: ChatSendBeforeEvent) => {
+    var freeCameraRunId: boolean | number | string | Vector3 | undefined = event.sender.getDynamicProperty("freeCameraRunId")
+    if ((freeCameraRunId !== undefined && freeCameraRunId !== 0) && event.message === "+fc") {
+        event.cancel = true;
+        system.run(() => {
+            system.clearRun(freeCameraRunId as number);
+            event.sender.setDynamicProperty("freeCameraRunId", 0)
+            event.sender.camera.clear();
+            event.sender.sendMessage("FreeCamera off");
+        });
+        return;
+    }
+    if ((freeCameraRunId === undefined || freeCameraRunId === 0) && event.message.startsWith("+fc ")) {
+        event.cancel = true;
+        system.run(() => {
+            var originLocation: Vector3 = event.sender.location;
+            var speed: number = parseFloat(event.message.substring(4));
+            var cameraY: number = originLocation.y;
+            if (isNaN(speed)) {
+                event.sender.sendMessage("§cBad Input: speed§r");
+                return;
+            }
+            event.sender.sendMessage(`FreeCamera on (speed: ${speed})`);
+            event.sender.setDynamicProperty("freeCameraRunId", system.runInterval(() => {
+                if (event.sender.isSneaking) { cameraY -= 0.5; }
+                if (!event.sender.isOnGround) { cameraY += 0.5; }
+                event.sender.camera.setCamera("minecraft:free", { easeOptions: { easeTime: 0.25, easeType: EasingType.Linear }, location: { x: originLocation.x + (event.sender.location.x - originLocation.x) * speed, y: cameraY, z: originLocation.z + (event.sender.location.z - originLocation.z) * speed }, rotation: event.sender.getRotation() });
+            }, 5));
+        });
+        return;
+    }
+});
