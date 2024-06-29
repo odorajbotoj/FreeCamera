a6db843fd837efee92e022b87add3f09c40938e7
odorajbotoj <odoraj_botoj@tom.com>
2024-06-29 16:38:29 +0800

    1.1.0

--- scripts/main.ts
+++ scripts/main.ts
@@ -3,17 +3,20 @@ import { world, ChatSendBeforeEvent, system, EasingType, Vector3, Player } from 
 
 //@ts-ignore
 world.beforeEvents.chatSend.subscribe((event: ChatSendBeforeEvent) => {
+    // get RunId
     var freeCameraRunId: boolean | number | string | Vector3 | undefined = event.sender.getDynamicProperty("freeCameraRunId");
-    if ((freeCameraRunId !== undefined && freeCameraRunId !== 0) && event.message === "+fc") {
+    // clear run
+    if (freeCameraRunId !== undefined && event.message === "+fc") {
         event.cancel = true;
+        system.clearRun(freeCameraRunId as number);
+        event.sender.setDynamicProperty("freeCameraRunId", 0);
         system.run(() => {
-            system.clearRun(freeCameraRunId as number);
-            event.sender.setDynamicProperty("freeCameraRunId", 0);
             event.sender.camera.clear();
             event.sender.sendMessage("FreeCamera off");
         });
         return;
     }
+    // free camera
     if ((freeCameraRunId === undefined || freeCameraRunId === 0) && event.message.startsWith("+fc ")) {
         event.cancel = true;
         system.run(() => {
@@ -30,9 +33,10 @@ world.beforeEvents.chatSend.subscribe((event: ChatSendBeforeEvent) => {
             event.sender.setDynamicProperty("freeCameraRunId", system.runInterval(() => {
                 cameraX += (event.sender.location.x - originLocation.x) * speed;
                 if (event.sender.isSneaking) { cameraY -= 0.5; }
-                if (!event.sender.isOnGround) { cameraY += 0.5; }
+                if (event.sender.isJumping) { cameraY += 0.5; }
                 cameraZ += (event.sender.location.z - originLocation.z) * speed;
-                event.sender.camera.setCamera("minecraft:free", { easeOptions: { easeTime: 0.25, easeType: EasingType.Linear }, location: { x: cameraX, y: cameraY, z: cameraZ }, rotation: event.sender.getRotation() });
+                try { event.sender.camera.setCamera("minecraft:free", { easeOptions: { easeTime: 0.25, easeType: EasingType.Linear }, location: { x: cameraX, y: cameraY, z: cameraZ }, rotation: event.sender.getRotation() }); }
+                catch (e) { }
                 event.sender.teleport(originLocation, { rotation: event.sender.getRotation() });
             }, 5));
         });
@@ -41,26 +45,24 @@ world.beforeEvents.chatSend.subscribe((event: ChatSendBeforeEvent) => {
 });
 
 world.afterEvents.entityHurt.subscribe((event) => {
-    world.sendMessage("debug1");
     var freeCameraRunId: boolean | number | string | Vector3 | undefined = event.hurtEntity.getDynamicProperty("freeCameraRunId");
     if (freeCameraRunId !== undefined && freeCameraRunId !== 0) {
+        system.clearRun(freeCameraRunId as number);
+        event.hurtEntity.setDynamicProperty("freeCameraRunId", 0);
         system.run(() => {
-            system.clearRun(freeCameraRunId as number);
-            event.hurtEntity.setDynamicProperty("freeCameraRunId", 0);
             (event.hurtEntity as Player).camera.clear();
             (event.hurtEntity as Player).sendMessage("FreeCamera off");
         });
         return;
     }
-});
+}, { entityTypes: ["minecraft:player"] });
 
 world.afterEvents.playerDimensionChange.subscribe((event) => {
-    world.sendMessage("debug2");
     var freeCameraRunId: boolean | number | string | Vector3 | undefined = event.player.getDynamicProperty("freeCameraRunId");
     if (freeCameraRunId !== undefined && freeCameraRunId !== 0) {
+        system.clearRun(freeCameraRunId as number);
+        event.player.setDynamicProperty("freeCameraRunId", 0);
         system.run(() => {
-            system.clearRun(freeCameraRunId as number);
-            event.player.setDynamicProperty("freeCameraRunId", 0);
             event.player.camera.clear();
             event.player.sendMessage("FreeCamera off");
         });
@@ -69,15 +71,23 @@ world.afterEvents.playerDimensionChange.subscribe((event) => {
 });
 
 world.afterEvents.entityDie.subscribe((event) => {
-    world.sendMessage("debug3");
     var freeCameraRunId: boolean | number | string | Vector3 | undefined = event.deadEntity.getDynamicProperty("freeCameraRunId");
     if (freeCameraRunId !== undefined && freeCameraRunId !== 0) {
+        system.clearRun(freeCameraRunId as number);
+        event.deadEntity.setDynamicProperty("freeCameraRunId", 0);
         system.run(() => {
-            system.clearRun(freeCameraRunId as number);
-            event.deadEntity.setDynamicProperty("freeCameraRunId", 0);
             (event.deadEntity as Player).camera.clear();
             (event.deadEntity as Player).sendMessage("FreeCamera off");
         });
+        return;
+    }
+}, { entityTypes: ["minecraft:player"] });
+
+world.beforeEvents.playerLeave.subscribe((event) => {
+    var freeCameraRunId: boolean | number | string | Vector3 | undefined = event.player.getDynamicProperty("freeCameraRunId");
+    if (freeCameraRunId !== undefined && freeCameraRunId !== 0) {
+        system.clearRun(freeCameraRunId as number);
+        event.player.setDynamicProperty("freeCameraRunId", 0);
         return;
     }
 });
